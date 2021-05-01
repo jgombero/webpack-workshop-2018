ac7fc33f9164699ed1041e03eb5088ec5df8d345
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-30 19:00:51 -0700

    Add css imports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import "./button.css";
+import "./footer.css";
 
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("magenta");
