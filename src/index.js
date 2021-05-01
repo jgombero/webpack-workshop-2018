b0bfa7938494f6b2273869f265372e27e5a93a18
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-30 18:25:07 -0700

    Append button and footer to document body

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import nav from "./nav";
+import { footer } from "./footer";
+import makeButton from "./button";
+import { makeColorStyle } from "./button-styles";
+
+const button = makeButton("Yay! A Button!");
+button.style = makeColorStyle("magenta");
+document.body.appendChild(button);
+document.body.appendChild(footer);
+
+console.log(
+  nav(),
+  top,
+  bottom,
+  makeButton("My first button!"),
+  makeColorStyle("cyan")
+);
