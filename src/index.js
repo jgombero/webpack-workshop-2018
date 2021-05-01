ef89f1654c20ec7244e1179fcedc6164d525ab0c
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-04-30 20:58:07 -0700

    Import and append image to document body

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import nav from "./nav";
-import { footer } from "./footer";
+import { footer, top, bottom } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import makeImage from "./image";
+import imageURL from "./webpack-logo.jpeg";
+
 import "./button.css";
 import "./footer.css";
 
+const image = makeImage(imageURL);
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("magenta");
+
 document.body.appendChild(button);
+document.body.appendChild(image);
 document.body.appendChild(footer);
 
 console.log(
